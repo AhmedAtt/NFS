8d1415d36d2435e2bf5020a715f7ae714babcb67
omargamal8 <omargamalh@hotmail.com>
2016-12-18 00:27:20 +0200

    new branch for english game

---
+++ SignIn.js
@@ -0,0 +1,15 @@
+(function(){
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDzWxlbKReTJQyvad5DLBJ6je5e4IDpUVQ",
+    authDomain: "agile-30622.firebaseapp.com",
+    databaseURL: "https://agile-30622.firebaseio.com",
+    storageBucket: "agile-30622.appspot.com",
+    messagingSenderId: "592891433441"
+  };
+  firebase.initializeApp(config);
+  //get elements
+  const txtEmail=document.getElementById('txtusername');
+  const txtPassword=document.getElementById('txtpassword');
+  const btnLogin=document.getElementById('btnlogin');
+  }());
