5a7ca5dc274128cd938582360108813a693aa107
RemonMelika <remon-1995@hotmail.com>
2016-12-24 04:14:21 +0200

    SignUpData2Firebase & Profile Edits
    -Creates Profiles object containing email(Primary Key) -> Name, ScoreM, ScoreE
    -Added Profile.js to allow Profile.html editing
    To Do: Push Profile data into Firebase & load Profile data from Firebase

--- JavaScript/indexUp.js
+++ JavaScript/indexUp.js
@@ -10,24 +10,37 @@
   firebase.initializeApp(config);
   
 //getElements
-
+const txtName=document.getElementById('txtName');
 const txtEmailUp=document.getElementById('txtEmailUp');
 const txtPasswordUp=document.getElementById('txtPasswordUp');
 const txtPasswordUp2=document.getElementById('txtPasswordUp2');
 const btnSignup=document.getElementById('btnSignup');
+//make emails format compatible with firebase
+function fixEmail(email) {
+  if (!email) return false
+  email = email.toLowerCase();
+  email = email.replace(/\./g, ',');
+  return email;
+}
 //signUpEvent
 btnSignup.addEventListener('click',e=>{
 const email=txtEmailUp.value;
 const password=txtPasswordUp.value;
+const name=txtName.value;
 const auth =firebase.auth();
-//signUp
+//signUp and createData
 if (txtPasswordUp2.value != txtPasswordUp.value){
+document.getElementById('pwerror').classList.remove('hide'); //remove pw mismatch
 console.log("Please confirm your password correctly");
 txtPasswordUp.value="";
-txtPasswordUp2.value="";
+txtPasswordUp2.value=""; //clear pw to re-enter
 }
 else{
+document.getElementById('pwerror').classList.add('hide'); //add pw mismatch
 const promise= auth.createUserWithEmailAndPassword(email,password);
+var profileRef = firebase.database().ref('Profiles'); //Create Profile object
+var profile = profileRef.child(fixEmail(email)); //Set Email as primary key
+profile.set({Email: email, Name: name, ScoreM: 0, ScoreE: 0 }); //Create paths inside profile
 promise.catch(e=>console.log(e.message));
 }
 });
@@ -36,7 +49,7 @@ firebase.auth().onAuthStateChanged(firebaseUser =>{
 	if(firebaseUser){
 		console.log(firebaseUser);
 		window.location.href = "Signed.html";
-		btnSignup.classList.add('hide');
+		btnSignup.classList.add('hide');+
 	}
 	else {
 		console.log('not logged in');
